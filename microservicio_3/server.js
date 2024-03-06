de61d3b699da79ce9df81072fe6206ea0f3b65fd
Feliciano Franco <f64franco@gmail.com>
2024-03-05 23:47:32 -0600

    + Microservice 3

--- microservicio_3/server.js
+++ microservicio_3/server.js
@@ -1,22 +1,37 @@
 const express = require("express");
-require('dotenv').config()
+require("dotenv").config();
 const app = express();
 const host = process.env.HOST;
 const port = process.env.PORT;
+const micro1Url = process.env.MICRO1_URL;
+const micro2Url = process.env.MICRO2_URL;
 
 // Endpoint para obtener datos
-app.get("/", (req, res) => {
-  fetch("http:localhost:3001?name=" + req.query.name)
+app.get("/", async (req, res) => {
+  const name = req.query.name;
+  if (!name) {
+    res.send({ message: "No se envió el parámetro 'name' en la URL" });
+  }
+  console.log(name);
+
+  let micro1, micro2;
+
+  await fetch(`${micro1Url}/?name=${req.query.name}`)
     .then((response) => response.json())
     .then((data) => {
-      res.send(data);
+      micro1 = data;
     });
 
-  fetch("http:localhost:3002?name=" + req.query.name)
+  await fetch(`${micro2Url}/?name=${req.query.name}`)
     .then((response) => response.json())
     .then((data) => {
-      res.send(data);
+      micro2 = data;
     });
+
+  res.send({
+    agify: micro1,
+    genderize: micro2,
+  });
 });
 
 app.listen(port, host, () => {
